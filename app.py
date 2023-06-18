d00e7ea24be4720cb05cb094a908ab289c17383c
Xusrav <xusrav200329@gmail.com>
2023-06-18 21:03:15 +0500

    Создал логику для получения рандомной цитаты и для него шаблон

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from key import KEY
+import requests
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = KEY
@@ -7,6 +8,13 @@ app.config['SECRET_KEY'] = KEY
 
 @app.route('/')
 def main():
+    response = requests.get("https://api.quotable.io/quotes/random").json()
+    context = {
+        'author': response[0]['author'],
+        'content': response[0]['content'],
+    }
+    return render_template('index.html', context=context)
 
-    return render_template('index.html')
 
+if __name__ == '__main__':
+    app.run(debug=True)
