1899ca11afae353bc8dd1476f05206789bb24420
Xusrav <xusrav200329@gmail.com>
2023-06-18 20:46:37 +0500

    Создал функцию и html шаблон для главной страницы

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+from key import KEY
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = KEY
+
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
